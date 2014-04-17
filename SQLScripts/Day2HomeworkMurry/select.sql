0eea87877b30aeda0fe731c648bb4a697f266f52
Flagship 2 <flagship2@charlestoncodes.com>
2014-04-16 20:45:18 -0400

    I CHANGED STUFF AT END OF CALSS

--- SQLScripts/Day2HomeworkMurry/select.sql
+++ SQLScripts/Day2HomeworkMurry/select.sql
@@ -5,3 +5,17 @@ select * from Individual;
 
 select ID, FIRSTNAME, LastName, BirthDate FROM Individual;
 
+select team.ID, team.TeamName,
+batting.TeamID, BATTING.BATTINGAVG
+from TEAM
+inner join BATTING on BATTING.TeamID = team.ID
+
+SHOW databases
+;
+SHOW tables;
+CD BASEBALL
+;
+USE BASEBALL
+~ LS
+
+select * from band;
